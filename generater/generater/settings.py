3d4a36a047774d3fc646f71434d43627a62c4006
Shubham Yadav <subhamyadav580@gmail.com>
2020-06-11 21:24:52 +0530

    adding heroku deployment settings

--- generater/generater/settings.py
+++ generater/generater/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-4o2k2im**x&z^r5!*@@e*d&%#x0o*nx4&gqgm34ek@$s#&!mc'
+SECRET_KEY = 'l82ur-3k0zv##re!xl3_cp+)3@nua-f^18j#wrb5$slb%vr9(!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
